11665b7a3726c82bc423a0238846a6b6917f47fc
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-02 23:24:45 -0300

    Feat: resolucao 1001

---
+++ BEGINNER/1001.ts
@@ -0,0 +1,12 @@
+// Leia 2 variáveis, denominadas A e B e faça a soma dessas duas variáveis, atribuindo seu resultado à variável X . Imprima X como mostrado abaixo. 
+
+
+
+function Sum (a:number, b:number) {
+    const X = a + b
+
+    return(` X = ${X} `);
+    
+}
+console.log(Sum(2, 4));
+
