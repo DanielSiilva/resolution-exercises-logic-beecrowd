4c572ea2c61cd3d61db7325278fbc67924159873
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-08 23:10:27 -0300

    Fix: interface

--- BEGINNER/1001.ts
+++ BEGINNER/1001.ts
@@ -1,8 +1,11 @@
 // Leia 2 variáveis, denominadas A e B e faça a soma dessas duas variáveis, atribuindo seu resultado à variável X . Imprima X como mostrado abaixo. 
 
+interface SomaPros{
+    a: number,
+    b: number
+}
 
-
-function Sum (a:number, b:number) {
+function Sum ({a, b}:SomaPros) {
     const X = a + b
 
     return(` X = ${X} `);
