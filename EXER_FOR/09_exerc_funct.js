a8a17805ff9ed00e6703767f1073ff9999ab59d4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-15 19:50:33 -0300

    Feat: adicionando função sobre conversão

---
+++ EXER_FOR/09_exerc_funct.js
@@ -0,0 +1,15 @@
+// Criar uma função que converta uma temperatura de Celsius para Fahrenheit 
+
+function convertCelsiusFahrenheit(n){
+    return n * 1.8 + 32;
+}
+
+console.log(`Meu resultado: ${convertCelsiusFahrenheit(100)}`);
+
+// ----------------------------------------------------------
+
+function celsiusParaFahrenheit(n) { 
+    return n * 1.8 + 32; 
+}
+
+console.log(`Meu resultado: ${convertCelsiusFahrenheit(100)}`);
