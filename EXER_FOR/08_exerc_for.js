52bb83674cb93cc8c35f2067c3d5bdb472448774
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-15 19:45:27 -0300

    Feat: adicionando exercicio 8 sobre o for

---
+++ EXER_FOR/08_exerc_for.js
@@ -0,0 +1,19 @@
+// Calcular a soma dos números impares maiores que 10 e menores que 30 
+
+// Declado um contador
+let cont = 0;
+
+for (let i = 11; i < 30; i++){
+    // verificar a contagem, a partit do loop, se o resto da divisão por 2 for igual a 2, será somado ao contador
+    if(i % 2 === 1){
+        cont += i
+    }
+}
+
+console.log(`Meu resultado: ${cont}`);
+
+var soma = 0; 
+for(var i = 11; i <= 30; i += 2) { 
+ soma += i; 
+} 
+console.log(`Gabarito: ${soma}`); 
