d98730757013384afc4717d9273988cd0371840c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-15 19:54:24 -0300

    Feat: adicionando exercicio 10 sobre funcao

---
+++ EXER_FOR/10_exerc_funct.js
@@ -0,0 +1,16 @@
+// Criar uma função que converta uma temperatura de Fahrenheit para Celsius
+
+function convertFahrenheitCelsius(n){
+    return (n - 32) / 1.8
+}
+
+console.log(`Meu resultado: ${convertFahrenheitCelsius(100)}`);
+
+
+// ----------------------------------------------
+
+function fahrenheitParaCelsius(n) { 
+    return (n - 32) / 1.8; 
+} 
+
+console.log(`Gabarito: ${fahrenheitParaCelsius(100)}`);
