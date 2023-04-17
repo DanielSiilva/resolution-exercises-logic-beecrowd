90deb26969c60b6cc85af3219f60c41a42480e9c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-17 11:56:25 -0300

    Feat: função calcula raiz quadrada

---
+++ EXER_FOR/19_exerc_for.js
@@ -0,0 +1,7 @@
+// Crie uma função que calcule a raiz quadrada de um número e retorne um número inteiro como resultado
+
+function squareRootOfNumber(n){
+    return parseInt(Math.sqrt(n))
+}
+
+console.log(`A raiz quadrada de ${4} é : ${squareRootOfNumber(4)}`);
